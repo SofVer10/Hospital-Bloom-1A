0c82c209e6ef3c4b58d49baf767dfa6014449674
Sofi <20230106@ricaldone.edu.sv>
2024-07-28 20:57:48 -0600

    tabla de oracle agregada en conexion

--- app/src/main/java/sofia/palacios/hospital_bloom/pacientes.kt
+++ app/src/main/java/sofia/palacios/hospital_bloom/pacientes.kt
@@ -1,32 +1,22 @@
 package sofia.palacios.hospital_bloom
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class bienvenida : AppCompatActivity() {
+class pacientes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bienvenida)
+        setContentView(R.layout.activity_pacientes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnContinuarBien = findViewById<Button>(R.id.btnContinuarBien)
 
-        btnContinuarBien.setOnClickListener{
-
-            val pantallaSiguiente = Intent(this@bienvenida, pacientes::class.java)
-            startActivity(pantallaSiguiente)
-
-            finish()
-        }
     }
 }
